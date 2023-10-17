dcf5f58bfef132b7bd5c4a667d8d9c3850ee9b58
FedeGG09 <124220922+FedeGG09@users.noreply.github.com>
2023-10-17 20:22:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,37 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from typing import List
 
 app = FastAPI()
 
-# Load the DataFrame from the CSV file
+# Cargar el DataFrame desde el archivo CSV
 df = pd.read_csv('MLRESULTS.csv')
 
-# Independent variables and dependent variable
+# Variables independientes y variable dependiente
 X = df[['Estado_California', 'Estado_Colorado', 'Estado_Georgia', 'Estado_NY',
        'Estado_Texas', 'Negocio_kfc', 'Sentimiento_Negativo', 'Sentimiento_Neutral',
        'Sentimiento_Positivo', 'Numero_reviews']]
 y = df['Calificacion_promedio']
 
-# Train the model
+# Entrenar el modelo
 model = LinearRegression()
 model.fit(X, y)
 
-class RestaurantScenario(BaseModel):
-    Estado: str  # Example: 'California'
-    Negocio: str  # Example: 'kfc'
+class EstadoInput(BaseModel):
+    Estado: str  # Ejemplo: 'California'
 
 @app.post('/predict/')
-def predict(data: List[RestaurantScenario]):
-    predictions = []
-    for scenario in data:
-        # Get the index corresponding to the state and restaurant chain
-        state_column = f'Estado_{scenario.Estado}'
-        chain_column = f'Negocio_{scenario.Negocio}'
+def predict(data: EstadoInput):
+    estado = data.Estado
 
-        input_data = [0] * len(X.columns)  # Initialize with zeros for all columns
-        input_data[X.columns.get_loc(state_column)] = 1  # Set the state column to 1
-        input_data[X.columns.get_loc(chain_column)] = 1  # Set the restaurant chain column to 1
+    if f'Estado_{estado}' not in X.columns:
+        return {"error": f"El estado {estado} no está en el conjunto de datos"}
 
-        prediction = model.predict([input_data])
-        predictions.append({
-            'Estado': scenario.Estado,
-            'Negocio': scenario.Negocio,
-            'prediction': prediction[0]
-        })
-    
-    return predictions
+    input_data = [0] * len(X.columns)  # Inicializar con ceros para todas las columnas
+    input_data[X.columns.get_loc(f'Estado_{estado}')] = 1  # Establecer la columna del estado en 1
+    input_data[X.columns.get_loc('Negocio_kfc')] = 1  # Establecer la columna de Negocio_kfc en 1 (siempre se asume kfc en este ejemplo)
+
+    prediction = model.predict([input_data])
+
+    return {"Estado": estado, "prediction": prediction[0]}
